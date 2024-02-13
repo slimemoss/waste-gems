f31a826648430f7492d6a4bb65dda86119e843d4
slimemoss <common.tken@gmail.com>
2024-02-14 07:07:29 +0900

    init commit

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Route, Routes, HashRouter } from 'react-router-dom'
+import { Helmet } from "react-helmet"
+import {Page} from './pages'
+
+const container = document.getElementById('app')
+if (!container) throw new Error('Failed to find the root element')
+const root = createRoot(container)
+
+root.render(
+  <>
+    <Helmet
+      title={''}
+    />
+
+    <HashRouter>
+      <Routes>
+        <Route path="*" element={<Page/>}/>
+      </Routes>
+    </HashRouter>
+  </>,
+)
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+export const Page = () => {
+  return (
+    <>
+      <div>
+        reactをgithub pagesでホストするためのテンプレートです。
+        <a href="https://github.com/slimemoss/react-template">ソースコードはここ</a>
+      </div>
+    </>
+  )
+}
